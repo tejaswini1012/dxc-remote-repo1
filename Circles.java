b741709ab94d5f8c9d2ada3e64ceffdf047f1f22
Bikki tejaswini <tejabikki1998@gmail.com>
2020-08-11 17:35:33 +0530

    first commit

---
+++ Circles.java
@@ -0,0 +1,12 @@
+public class Circles
+{
+   double radius;
+   public Circle(double radius)
+   {
+      this.radius=radius;
+   }
+   public double area()
+   {
+      return 3.14*radius*radius;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class main
+{
+   public static void main(String[] args)
+   {
+      Circle c= new circle(4.5);
+      System.out.println(c.area());
+    }
+}
+   
